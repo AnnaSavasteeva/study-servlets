ac225d9b87501b57da13ac2c024a09fa4f63e152
annasavasteeva <anna.savasteeva@gmail.com>
2026-06-27 15:45:35 +0300

    Delete redundant imports

--- src/main/java/com/studyservlets/studyservlets/time_in_cities/BeijingTimeServlet.java
+++ src/main/java/com/studyservlets/studyservlets/time_in_cities/BeijingTimeServlet.java
@@ -4,7 +4,6 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import util.DateTimeUtil;
 
 import java.io.IOException;
 import java.time.ZoneId;
