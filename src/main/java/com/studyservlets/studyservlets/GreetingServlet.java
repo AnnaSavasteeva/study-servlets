ae00d6297b07e4d47715f32e04b409c5e9c293fe
annasavasteeva <anna.savasteeva@gmail.com>
2026-06-27 20:40:36 +0300

    Add second path to GreetingServlet

--- src/main/java/com/studyservlets/studyservlets/GreetingServlet.java
+++ src/main/java/com/studyservlets/studyservlets/GreetingServlet.java
@@ -1,18 +1,21 @@
 package com.studyservlets.studyservlets;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-
-@WebServlet("/greeting") // Этот сервлет обрабатывает запрос, когда в браузере вводится http://localhost:8081/greeting
+/*
+// Этот сервлет обрабатывает одинаково несколько запросов:
+- когда в браузер вводится http://localhost:8081/greeting
+- когда в браузер вводится http://localhost:8081/hello
+ */
+@WebServlet(urlPatterns = {"/greeting", "/hello"})
 public class GreetingServlet extends HttpServlet {
 
     @Override // Этот метод обрабатывает GET-запрос для ручки http://localhost:8081/greeting?name=Denis&age=22
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String name = req.getParameter("name");
         String age = req.getParameter("age");
 
